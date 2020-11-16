91becbb18aac1fa04ab56c9b759c17e14b0a1156
arjmemc05 <74553763+arjmemc05@users.noreply.github.com>
2020-11-16 21:51:20 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1467820292:AAGzzv9XWEqNLNgNl_oMZo-UdunrHzcUYBgc"
+    APP_ID = "2668427"
+    API_HASH = "a5ddb6ef5cc0620bf81fc14babb31993"
     DATABASE_URL = "postgres://ldbyfrgjyentuv:a70f5525b37da67a33958f851706bbaddcbf29b977fa09be59b6ec2c1a8db096@ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/d8eoi8a3gduf6"
     SUDO_USERS = "1481489037" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/WE26jRPX37_dq3oJ9Woqzw"
